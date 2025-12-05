b63a50b83df919dc69d52b84ded66000c5938071
Hasan Mukhlis <hsnmkls@proton.me>
2025-12-05 23:06:37 +0800

    Fix parent-child mixed communication

--- src/parent.ts
+++ src/parent.ts
@@ -1,48 +1,47 @@
 import type * as Mipd from "mipd";
 import type { RpcRequest } from "ox/RpcRequest";
 import type { RpcResponse } from "ox/RpcResponse";
+import { nanoid } from "nanoid";
 
 export type ChildMessageSchema =
   | {
-      type: "eip6963:requestProvider";
-    }
+    type: "eip6963:requestProvider";
+  }
   | {
-      type: "rpcrequest";
-      providerUuid: string;
-      request: RpcRequest;
-    }
+    type: "rpcrequest";
+    providerUuid: string;
+    request: RpcRequest;
+  }
   | {
-      type: "rpcsubscribe";
-      providerUuid: string;
-      event: string;
-      handlerId: string;
-    }
+    type: "rpcsubscribe";
+    providerUuid: string;
+    event: string;
+    handlerId: string;
+  }
   | {
-      type: "rpcunsubscribe";
-      providerUuid: string;
-      event: string;
-      handlerId: string;
-    };
+    type: "rpcunsubscribe";
+    providerUuid: string;
+    event: string;
+    handlerId: string;
+  };
 
 export type ParentMessageSchema =
   | {
-      type: "eip6963:announceProvider";
-      info: Mipd.EIP6963ProviderInfo;
-    }
+    type: "eip6963:announceProvider";
+    info: Mipd.EIP6963ProviderInfo;
+  }
   | {
-      type: "rpcresponse";
-      providerUuid: string;
-      response: RpcResponse;
-    }
+    type: "rpcresponse";
+    providerUuid: string;
+    response: RpcResponse;
+  }
   | {
-      type: "rpcevent";
-      providerUuid: string;
-      handlerId: string;
-      event: string;
-      data: any;
-    };
-
-const exposed = new WeakSet<Window>();
+    type: "rpcevent";
+    providerUuid: string;
+    handlerId: string;
+    event: string;
+    data: any;
+  };
 
 export function expose(parameters: {
   store: Mipd.Store;
@@ -51,16 +50,24 @@ export function expose(parameters: {
 }) {
   const { store, child, origin } = parameters;
 
-  if (exposed.has(child)) return;
-  exposed.add(child);
-
   const subscribers = new Map<string, (data: any) => void>();
+  const providers = new Map<string, Mipd.EIP6963ProviderDetail>();
 
-  const postInfo = (info: Mipd.EIP6963ProviderInfo) => {
+  const setAndPostProvider = (provider: Mipd.EIP6963ProviderDetail) => {
+    if (providers.values().find((p) => p.info.uuid === provider.info.uuid)) {
+      // we've already posted this provider
+      return;
+    }
+
+    const id = nanoid();
+    providers.set(id, provider);
     child.postMessage(
       {
         type: "eip6963:announceProvider",
-        info,
+        info: {
+          ...provider.info,
+          uuid: id
+        }
       } satisfies ParentMessageSchema,
       origin,
     );
@@ -74,17 +81,14 @@ export function expose(parameters: {
       }
 
       if (event.data.type === "eip6963:requestProvider") {
-        const providers = store.getProviders();
-        for (const provider of providers) {
-          postInfo(provider.info);
+        for (const provider of store.getProviders()) {
+          setAndPostProvider(provider);
         }
       }
 
       if (event.data.type === "rpcrequest") {
         const { providerUuid, request } = event.data;
-        const provider = store
-          .getProviders()
-          .find((p) => p.info.uuid === providerUuid);
+        const provider = providers.get(providerUuid);
         if (!provider) return;
 
         try {
@@ -170,7 +174,7 @@ export function expose(parameters: {
 
   store.subscribe((providers) => {
     for (const provider of providers) {
-      postInfo(provider.info);
+      setAndPostProvider(provider);
     }
   });
 }
